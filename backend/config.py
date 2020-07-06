7fa1d273a7be16c2273ad7a19488cafce31fc8ab
stephaniekwan <steffy.kwan@gmail.com>
2020-07-06 00:58:49 -0700

    Redid backend; Flask instead of Node

--- backend/config.py
+++ backend/config.py
@@ -4,18 +4,17 @@ import pyrebase
 #from flask_api import FlaskAPI
 
 # init firebase connection to database using pyrebase
-firebase = pyrebase.initialize_app (
-    {
-        apiKey: "AIzaSyDXQDNAFdn5Qj_qHf3uJfiaAYy17bPquGw",
-        authDomain: "sdsc-fake-news.firebaseapp.com",
-        databaseURL: "https://sdsc-fake-news.firebaseio.com",
-        projectId: "sdsc-fake-news",
-        storageBucket: "sdsc-fake-news.appspot.com",
-        messagingSenderId: "948422758469",
-        appId: "1:948422758469:web:67bb4ec77cde4ce524532c",
-        measurementId: "G-XZ1C5TYLDF"
-    }
-)
+config = {
+    apiKey: "AIzaSyDXQDNAFdn5Qj_qHf3uJfiaAYy17bPquGw",
+    authDomain: "sdsc-fake-news.firebaseapp.com",
+    databaseURL: "https://sdsc-fake-news.firebaseio.com",
+    projectId: "sdsc-fake-news",
+    storageBucket: "sdsc-fake-news.appspot.com",
+    messagingSenderId: "948422758469",
+    appId: "1:948422758469:web:67bb4ec77cde4ce524532c",
+    measurementId: "G-XZ1C5TYLDF"
+}
+firebase = pyrebase.initialize_app(config)
 
 # create variable to access firebase DB
 db = firebase.database()
