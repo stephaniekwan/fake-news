e0df3b96c8155917f429f06ed67fcae1f92287ff
stephaniekwan <steffy.kwan@gmail.com>
2020-07-06 12:45:41 -0700

    Added server IP

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from app import app
 
 if __name__ == "__main__":
+    # Local Host
     #app.run(host='127.0.0.1', port=5000)
-    app.run(port=5000)
+
+    # Server
+    app.run(host='132.249.238.69', port=5000)
+    
