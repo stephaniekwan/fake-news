5f6f6c12ded131de74d6d62b02fd2d14b587d545
stephaniekwan <steffy.kwan@gmail.com>
2020-07-06 13:31:07 -0700

    Localhost and server for app

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 from app import app
 
 if __name__ == "__main__":
-    # Local Host
-    #app.run(host='127.0.0.1', port=5000)
+    # Works for local host and server
+    # Server IP: IP = 132.249.238.69; local host: 127.0.0.1
+    app.run(host='0.0.0.0', port=5000)
 
-    # Server
-    app.run(host='132.249.238.69', port=5000)
     
