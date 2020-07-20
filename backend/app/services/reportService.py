7031e009274ffd81dfda0fca80bf22604c6ac731
djuanit0x <dennisjuanito@gmail.com>
2020-07-20 08:20:18 -0700

    Increased the limit of queried reports from one user_id

--- backend/app/services/reportService.py
+++ backend/app/services/reportService.py
@@ -63,7 +63,7 @@ def get_report_by_user_id(user_id):
 
 
     # Get a single report that matches the user_id
-    doc_ref_gen = reports_ref.where(u'user_id', u'==', user_id).limit(1).stream()
+    doc_ref_gen = reports_ref.where(u'user_id', u'==', user_id).limit(100).stream()
 
     # Get the document snapshot of the doc_ref_gen generator
     doc_ref = next(doc_ref_gen, None)
