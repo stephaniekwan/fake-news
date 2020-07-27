ba87016b13866e158bfa2e1d0a860a516b16d160
stephaniekwan <steffy.kwan@gmail.com>
2020-07-27 01:17:04 -0700

    Added a json request const, needs testing/implementation

---
+++ client/src/utils/request.js
@@ -0,0 +1,34 @@
+const request = async (path, options, headers = false) => {
+
+    // TODO: update this variable bc we dont currently use docker
+    const URL = process.env.NODE_ENV === 'production' || process.env.REACT_APP_VARIABLE === 'docker' ? 
+                '' : 'http://localhost:3001'
+  
+    options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      ...options
+    }
+  
+    if (options.body)
+      options.body = JSON.stringify(options.body)
+  
+    const response = await fetch(URL + path, options)
+    const body = await response.json()
+    if (response.status === 404) {
+      if (body.code) {
+        throw body.code
+      } else {
+        throw new Error()
+      }
+    }
+  
+  
+    if(headers)
+      return {body, headers: response.headers}
+  
+    return body
+  }
+  
+  export default request
