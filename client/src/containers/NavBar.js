a3890a3f1fb17bea735c438241ca2d6c60a21254
djuanit0x <dennisjuanito@gmail.com>
2020-07-29 09:43:54 -0700

    Implemented a localstorage to check the existential of userid

--- client/src/containers/NavBar.js
+++ client/src/containers/NavBar.js
@@ -1,45 +1,63 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import '../styles/NavBar.css'
+import React, {useEffect, useState} from "react";
+import {Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
+import "../styles/NavBar.css";
 //import Report from '../components/Report'
 //import App from '../components/App'
 
 /*
  * Define the bar at the top of each screen
  */
-function NavBar() {
-    //const [key, setKey] = React.useState('App');
+export default () => {
+    const [isLoggedIn, setLoggedIn] = useState(null);
 
-    const height = '100px'
+    const height = "100px";
 
-    
+    useEffect(() => {
+        setLoggedIn({
+            isLoggedIn:
+                typeof window !== "undefined" &&
+                window.localStorage.getItem("user_id") !== null
+        });
+
+        if (
+            typeof window !== "undefined" &&
+            window.localStorage.getItem("user_id") == null
+        ) {
+            localStorage.setItem('user_id', uuidv4());
+        }
+    }, []);
+
     return (
-        <div id='navbar-div' style={{ height, backgroundColor: 'rgb(31, 70, 129)'}}>
+        <div id='navbar-div' style={{height, backgroundColor: "rgb(31, 70, 129)"}}>
             <Navbar className='navbar'>
-                
                 {
-                <>
-                    <div className='centered'>
-                        <Link to='/'>
-                            <h1 className='Title'>S T O N K S</h1>
-                        </Link>
-                    </div>
-                    <div className='right'>
-                        <Link to='/faq'>
-                            <input className='Info' type='image' 
-                            src='./assets/infobutton.png' alt='./assets/infobutton2.png' />
-                        </Link>
-                    </div>
-                </> 
+                    <>
+                        <div className='centered'>
+                            <Link to='/'>
+                                <h1 className='Title'>S T O N K S</h1>
+                            </Link>
+                        </div>
+                        <div className='right'>
+                            <Link to='/faq'>
+                                <input
+                                    className='Info'
+                                    type='image'
+                                    src='./assets/infobutton.png'
+                                    alt='./assets/infobutton2.png'
+                                />
+                            </Link>
+                        </div>
+                    </>
                 }
             </Navbar>
         </div>
-    )
+    );
 
-    /* 
+    /*
     <button><img src='./assets/infobutton.png'
-                            alt='./assets/logo-small.png' 
+                            alt='./assets/logo-small.png'
                             className='Info' onClick={onClick} /></button>
 
     return (
@@ -54,5 +72,3 @@ function NavBar() {
     );
     */
 }
-
-export default NavBar;
