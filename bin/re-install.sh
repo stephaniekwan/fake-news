6e55ea061d4dd1761d8bb0dc3e383d1d8470539d
stephaniekwan <steffy.kwan@gmail.com>
2020-07-29 11:18:58 -0700

    Updated and combined the 2 setup scripts

--- bin/re-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cd backend; pip install -r requirements.txt; cd -
-cd client; npm i; cd -
--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+
 echo -e "Installing dependencies\n"
 
-npm --prefix client/ install
-pip3 install -r backend/requirements.txt --user 
+cd backend; pip3 install -r requirements.txt; cd -
+cd client; npm i; cd -
 
 echo -e "\nTo run:"
 echo "1. cd into the client directory"
