cc3ff44ca2b9f128afade898a37b27306f6e7529
stephaniekwan <steffy.kwan@gmail.com>
2020-07-29 11:38:55 -0700

    Updated instructions with concurrently command

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ cd client; npm i; cd -
 echo -e "\nTo run:"
 echo "1. cd into the client directory"
 echo "2. type 'npm run build' to compile"
-echo "3. type 'npm start' to run"
+echo "3. type 'npm start' to run the frontend"
+echo "Alternatively, type 'npm run concurrently' while in client directory to run the front and backend"
