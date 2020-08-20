f2532d972c98334293f51ea1246924ecad81ca43
ameyasingh <ameyasingh37@gmail.com>
2020-08-20 17:22:45 +0000

    Backend for percentage added

--- Scripts/app.py
+++ Scripts/app.py
@@ -25,7 +25,22 @@ def get_delay():
     pred = pipeline.predict(query)
     print(pred)
     dic = {0:'real',1:'fake'}
-    return f'<html><body><h1>{dic[pred[0]]}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
+    mResult = pred[0]
+    mAccuracy = pipeline.predict(query)
+    if (pred[0] == 1):
+        mPerc = 0
+    elif (pred[0] == 0):
+        if(len(mAccuracy) % 2 == 0):
+            mPerc = 2
+        else:
+            mPerc = 1
+    if(mPerc == 0):
+        mString = "< 50%"
+    elif(mPerc == 1):
+        mString = "60% - 75%"
+    elif(mPerc == 2):
+        mString = "> 80%"
+    return f'<html><body><h1>{dic[pred[0]]} {mString}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
 
 
 if __name__ == '__main__':
