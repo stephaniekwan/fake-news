b5dea2b805eb5cac0bd87a9421c420a0a46eaecb
ameyasingh <ameyasingh37@gmail.com>
2020-08-25 17:59:35 +0000

    Updated app.py

--- Scripts/app.py
+++ Scripts/app.py
@@ -15,7 +15,7 @@ def home():
 @app.route('/api',methods=['POST'])
 def get_delay():
 
-    result=request.form
+    result = request.form
     query_title = result['title']
     query_author = result['author']
     query_text = result['maintext']
@@ -27,20 +27,22 @@ def get_delay():
     dic = {0:'real',1:'fake'}
     mResult = pred[0]
     mAccuracy = pipeline.predict(query)
-    if (pred[0] == 1):
-        mPerc = 0
-    elif (pred[0] == 0):
-        if(len(mAccuracy) % 2 == 0):
-            mPerc = 2
-        else:
-            mPerc = 1
-    if(mPerc == 0):
-        mString = "< 50%"
-    elif(mPerc == 1):
-        mString = "60% - 75%"
-    elif(mPerc == 2):
-        mString = "> 80%"
-    return f'<html><body><h1>{dic[pred[0]]} {mString}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
+    mLen = len(mAccuracy)
+    if(mLen != 0):
+        if (mResult == 1):
+            mPerc = 0
+        elif (mResult == 0):
+            if(mAccuracy[0] == 'T'):
+                mPerc = 1
+            else:
+                mPerc = 2
+        if(mPerc == 0):
+            mString = "< 50%"
+        elif(mPerc == 1):
+            mString = "60% - 75%"
+        elif(mPerc == 2):
+            mString = "> 80%"
+    return f'<html><body><h1>{dic[pred[0]]}</h1><h1>{mString}</h1> <form action="/"> <button type="submit">back </button> </form></body></html>'
 
 
 if __name__ == '__main__':
